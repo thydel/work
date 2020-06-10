b3c1cc0ce42ca7d43a424a9e14d998d18c94af67
Thierry Delamare <t.delamare@laposte.net>
2020-06-10 19:03:05 +0200

    Makes it work with jq-1.6 (instead of compiled from master)

--- groups.mk
+++ groups.mk
@@ -4,7 +4,7 @@ find :=   find ext -type f -name '*.mk'
 find += | sed -e s/^ext.//
 
 jq :=   map(split("/"))
-jq += | reduce .[] as $$i ({}; .[$$i[-1]] += ($$i[0:-1] | reduce .[] as $$i ({}; . += { $$i: null })))
+jq += | reduce .[] as $$i ({}; .[$$i[-1]] += ($$i[0:-1] | reduce .[] as $$i ({}; . += { ($$i): null })))
 jq += | with_entries(.value = (.value | keys))
 
 # https://github.com/stedolan/jq/issues/243
